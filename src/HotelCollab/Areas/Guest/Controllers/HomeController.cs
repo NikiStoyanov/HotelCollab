31515b5ff3f9f607edf8712bbaa4d71f8c0bd467
Nikolay Stoyanov <48627318+NikolayStoyanov05@users.noreply.github.com>
2021-02-24 10:36:13 +0200

    Changes

--- src/HotelCollab/Areas/Guest/Controllers/HomeController.cs
+++ src/HotelCollab/Areas/Guest/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +9,23 @@ namespace HotelCollab.Areas.Guest.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize]
         [Area("Guest")]
         public IActionResult Reservation()
+        {
+            return this.View();
+        }
+
+        [Authorize]
+        [Area("Guest")]
+        public IActionResult Signal()
+        {
+            return this.View();
+        }
+
+        [Authorize]
+        [Area("Guest")]
+        public IActionResult Events()
         {
             return this.View();
         }
